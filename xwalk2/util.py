5aa32950c84fc148bffd0eea09e88490ec9b72d9
Colin Roberts <7387323+colinjroberts@users.noreply.github.com>
2025-06-01 21:56:38 -0700

    Add a stop method to heartbeat so I can kill it

--- xwalk2/util.py
+++ xwalk2/util.py
@@ -6,16 +6,29 @@ from datetime import datetime
 
 
 def heatbeat(component, host, every_s=10):
-  def _beat():
-    context = zmq.Context.instance()
-    socket = context.socket(zmq.PUB)
-    socket.connect("tcp://127.0.0.1:5558")
-    while True:
-      print(f"{component} beating")
-      socket.send_string(Heatbeat(host=host, component=component, sent_at=datetime.now()).model_dump_json())
-      time.sleep(every_s)
+    stop_event = threading.Event()
 
-  thread = threading.Thread(target=_beat)
-  thread.start()
-  return thread
+    def _beat():
+        context = zmq.Context.instance()
+        socket = context.socket(zmq.PUB)
+        socket.connect("tcp://127.0.0.1:5558")
+        try:
+            while not stop_event.is_set():
+                print(f"{component} beating")
+                socket.send_string(Heatbeat(host=host, component=component, sent_at=datetime.now()).model_dump_json())
+                time.sleep(every_s)
+        finally:
+            socket.close()
+            context.term()
+
+    thread = threading.Thread(target=_beat)
+    thread.daemon = True
+    thread.start()
+
+    def stop():
+        stop_event.set()
+        thread.join(timeout=1.0)
+
+    thread.stop = stop
+    return thread
 
