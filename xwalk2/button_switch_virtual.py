f9ce7c3bd9f4ab023aae40a2fab14774e8e31843
Colin Roberts <7387323+colinjroberts@users.noreply.github.com>
2025-07-04 14:21:49 -0700

    Update virtual components

--- xwalk2/button_switch_virtual.py
+++ xwalk2/button_switch_virtual.py
@@ -1,8 +1,12 @@
+import argparse
+import logging
 import time
 from datetime import datetime
 
 from xwalk2.models import ButtonPress
-from xwalk2.util import InteractComponent
+from xwalk2.util import InteractComponent, add_default_args
+
+logger = logging.getLogger(__name__)
 
 
 class VirtualButton(InteractComponent):
@@ -10,21 +14,22 @@ class VirtualButton(InteractComponent):
         self,
         component_name: str,
         host_name: str,
-        interact_address="tcp://127.0.0.1:5556",
+        interact_address: str,
+        heartbeat_address: str,
     ) -> None:
-        super().__init__(component_name, host_name, interact_address)
+        super().__init__(component_name, host_name, interact_address, heartbeat_address)
+        logger.info("Initialized virtual button")
 
     def loop(self):
         while True:
             input("Press enter to simulate button press down")
-            print("Button pressed")
+            logging.info("Button pressed")
             press_start = time.time()
 
             input("Press enter to simulate button release")
             press_duration = int(
                 (time.time() - press_start) * 1000
             )  # Convert to milliseconds
-            print(f"Button released (held for {press_duration}ms)")
 
             button_press = ButtonPress(
                 host=self.host_name,
@@ -32,9 +37,19 @@ class VirtualButton(InteractComponent):
                 press_duration=press_duration,
                 sent_at=datetime.now(),
             )
+            logging.info(f"button press duration={press_duration}ms")
             self.send_action(button_press)
 
 
 if __name__ == "__main__":
-    button = VirtualButton("button_virtual", "crosswalk-a")
+    parser = argparse.ArgumentParser()
+    add_default_args(parser)
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=getattr(logging, args.log_level),
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    button = VirtualButton(
+        "button_virtual", args.hostname, args.interaction, args.heartbeat
+    )
     button.run()
